10071921f004497af0b9f0573fa12ce6020d8809
Paskudas <77464661+Paskudas@users.noreply.github.com>
2023-06-04 08:09:44 +0300

    +

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
+import notiflix from 'notiflix';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const { delay, step, amount } = event.target.elements;
+  const firstDelay = parseInt(delay.value);
+  const delayStep = parseInt(step.value);
+  const promiseAmount = parseInt(amount.value);
+
+  let currentDelay = firstDelay;
+
+  for (let i = 1; i <= promiseAmount; i++) {
+    createPromise(i, currentDelay)
+      .then(({ position, delay }) => {
+        notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    currentDelay += delayStep;
   }
 }
